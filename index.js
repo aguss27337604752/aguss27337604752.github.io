41cfbcc8d9a9b5b579c77db1f2ebb18b962badd0
aguss27337604752 <126636419+aguss27337604752@users.noreply.github.com>
2023-03-08 22:59:01 -0300

    commit init

---
+++ index.js
@@ -0,0 +1,31 @@
+document.getElementById ("dni").onclick=function(){
+    console.log("documento nacional de identidad");
+    document.getElementById("info").innerHTML="DNI: 33760475"
+  }
+document.getElementById ("direccion").onclick=function(){
+    console.log("direccion personal");
+    document.getElementById("info").innerHTML="DIRECCION: Charcas 523 BºEPA"
+}
+  
+
+  document.getElementById ("personal").onclick=function(){
+    console.log("email");
+    document.getElementById("info").innerHTML="EMAIL: agusmunoz024@gmail.com"
+  }
+
+  document.getElementById ("celular").onclick=function(){
+    console.log("numero de celular");
+    document.getElementById("info").innerHTML="CELULAR: 2617043652"
+  }
+  document.write (new Date());
+  let now =
+document.getElementById("tiempo").$tiempo=function Relojdigital(){
+  console.log("hora");
+}
+document.getElementById("color_fondo").addEventListener('click' ,function(){
+  document.body.style.backgroundColor="white"
+});
+
+document.getElementById("reestablecer").addEventListener('click', function(){
+  document.body.style.backgroundColor="rgb(214, 188, 219)"});
+
